a73fc938d7ec87b87471a4415cf400a9ca82ad74
Muhammad Zidan Khalil Gibran <zidangibran14@gmail.com>
2020-08-18 13:10:39 +0700

    initial commit

--- assets/script/script.js
+++ assets/script/script.js
@@ -61,3 +61,39 @@ function goTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+       $.getJSON('https://coronavirus-19-api.herokuapp.com/countries/Indonesia', function(data) {
+        
+        const kasusTotal = `${data.cases}`
+        const kasusHari = `${data.todayCases}`
+        const kasusJuta = `${data.casesPerOneMillion}`
+        const korbanTotal = `${data.deaths}`
+        const korbanHari = `${data.todayDeaths}`
+        const korbanJuta = `${data.deathsPerOneMillion}`
+        const tesTotal = `${data.totalTests}`
+        const tesJuta = `${data.testsPerOneMillion}`
+        const sembuh = `${data.recovered}`
+        const aktif = `${data.active}`
+        
+        $(".totalCases").html(Intl.NumberFormat('de-DE').format(kasusTotal));
+        $(".todayCases").html(Intl.NumberFormat('de-DE').format(kasusHari));
+        $(".casesPerMillion").html(Intl.NumberFormat('de-DE').format(kasusJuta));
+        $(".deaths").html(Intl.NumberFormat('de-DE').format(korbanTotal));
+        $(".todayDeaths").html(Intl.NumberFormat('de-DE').format(korbanHari));
+        $(".deathsPerMillion").html(Intl.NumberFormat('de-DE').format(korbanJuta));
+        $(".totalTests").html(Intl.NumberFormat('de-DE').format(tesTotal));
+        $(".testsPerMillion").html(Intl.NumberFormat('de-DE').format(tesJuta));
+        $(".recovered").html(Intl.NumberFormat('de-DE').format(sembuh));
+        $(".active").html(Intl.NumberFormat('de-DE').format(aktif));
+
+        const tanggal = new Date();
+
+        const thn = tanggal.getFullYear();
+        const bln = tanggal.getMonth() + 1;
+        const tgl = tanggal.getDate();
+
+        const tglformat = `Per tanggal ${tgl}-${bln}-${thn}` 
+
+        $(".tanggal").html(tglformat);
+
+      });
